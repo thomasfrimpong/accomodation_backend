75a70b482940f1e8c01d969d94d00f3883fdaabc
Thomas Frimpong <tfrimpong1987@gmail.com>
2023-04-21 09:36:05 +0000

    functions for reports on rentals

---
+++ app/Http/Controllers/ReportsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Classes\Data;
+use Illuminate\Http\Request;
+
+class ReportsController extends Controller
+{
+    //
+    public function sumOfExpenses(Request $request)
+    {
+        return Data::totalExpenses($request);
+    }
+
+
+    public function sumOfPayments(Request $request)
+    {
+        return Data::totalPayments($request);
+    }
+
+
+    public function sumOfRefunds(Request $request)
+    {
+        return Data::totalRefunds($request);
+    }
+
+    public function numberOfGuests(Request $request)
+    {
+        return Data::countOfGuests($request);
+    }
+
+    public function numberOfRentals(Request $request)
+    {
+        return Data::countOfRentals($request);
+    }
+}
